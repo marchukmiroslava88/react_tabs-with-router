d599bae6d38b769a551e075d3dc134d5f771215e
Miroslava <777shadowpriest777@gmail.com>
2019-10-02 23:40:21 +0300

    Updates

---
+++ precache-manifest.eea00d2afadc9a00d0620f54d6f8d44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6e9fdfbad98818d1b532d797aadfb6",
+    "url": "/marchukmiroslava88/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "4f6627371e38db9a9163",
+    "url": "/marchukmiroslava88/react_tabs-with-router/static/css/main.d5c6fffc.chunk.css"
+  },
+  {
+    "revision": "08063d9e1daa5585d7d1",
+    "url": "/marchukmiroslava88/react_tabs-with-router/static/js/2.66664ad5.chunk.js"
+  },
+  {
+    "revision": "4f6627371e38db9a9163",
+    "url": "/marchukmiroslava88/react_tabs-with-router/static/js/main.5d9a90e0.chunk.js"
+  },
+  {
+    "revision": "a11156ad2f9f5dab61b4",
+    "url": "/marchukmiroslava88/react_tabs-with-router/static/js/runtime~main.45f8363b.js"
+  }
+]);
